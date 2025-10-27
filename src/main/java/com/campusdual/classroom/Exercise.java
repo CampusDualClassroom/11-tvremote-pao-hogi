daa7496a2b8caec5d2afa2e1825a81ca3f433b1c
Paola Holguin Giraldo <pao.hogi@gmail.com>
2025-10-27 16:58:54 +0100

    Solved Exercise 11

--- src/main/java/com/campusdual/classroom/Exercise.java
+++ src/main/java/com/campusdual/classroom/Exercise.java
@@ -6,5 +6,13 @@ public class Exercise {
         TVRemote blackRemote = new TVRemote("Negro");
         System.out.println(redRemote.getColor());
         System.out.println(blackRemote.getColor());
+        redRemote.turnOn();
+        redRemote.channelUp();
+        System.out.println(redRemote.channel);
+        redRemote.volumeUp();
+        System.out.println(redRemote.volume);
+        redRemote.volumeDown();
+        redRemote.volume = 0;
+        redRemote.volumeDown();
     }
 }
--- src/main/java/com/campusdual/classroom/TVRemote.java
+++ src/main/java/com/campusdual/classroom/TVRemote.java
@@ -11,34 +11,61 @@ public class TVRemote {
         this.channel = 0;
         this.volume = 20;
         this.color = color;
+        this.on = false;
     }
 
     public boolean turnOn() {
-
+        if (!on) {
+            on = true;
+            System.out.println("TV encendida.");
+            return true;
+        } else {
+            System.out.println("La TV ya está encendida.");
+            return false;
+        }
     }
 
     public boolean turnOff() {
-
+        if (on) {
+            on = false;
+            System.out.println("TV apagada.");
+            return  true;
+        } else {
+            System.out.println("La TV ya está apagada.");
+            return false;
+        }
     }
 
     public void channelUp() {
-
-    }
+            channel++;
+            System.out.println("Canal subido a: " + channel);
+            }
 
     public void channelDown() {
-
+        if (channel > 0) {
+            channel--;
+            System.out.println("Canal bajado a: " + channel);
+        }else {
+            System.out.println("No se puede bajar el canal mas. ya está en 0. ");
+        }
     }
 
     public void volumeUp() {
-      
-    }
+             volume++;
+            System.out.println("Volumen subido a: " + volume);
+           }
 
     public void volumeDown() {
-
-    }
+             if (volume > 0) {
+                volume--;
+                System.out.println("Volumen bajado a : " + volume);
+            } else {
+                System.out.println("No se puede bajar más el volumen. ya está en 0.");
+            }
+        }
 
     public String getColor() {
-
+        return color;
     }
 
     private boolean checkMinValue(int value) {
